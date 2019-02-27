abe8e96da5024f37648495ff6c4ead52d838213f
DaviMenezes <davimenezes.dev@gmail.com>
2019-02-27 13:43:38 -0300

    melhorias nos helpers
    agora é o usuário dev que informa os parâmetros que definem o ambiente de edição

--- src/helpers/general.php
+++ src/helpers/general.php
@@ -1,9 +1,64 @@
+<?php
+
+/**
+ *  helpers
+ *
+ * @package
+ * @subpackage
+ * @author     Davi Menezes
+ * @copyright  Copyright (c) 2018. (davimenezes.dev@gmail.com)
+ * @see https://github.com/DaviMenezes
+ * @see https://t.me/davimenezes
+ */
+
+use Dvi\Support\Http\Request;
+use Dvi\Support\Collection;
+use Dvi\Support\Corda;
+use Dvi\Support\Http\Web;
+
+function collection($value)
+{
+    return new Collection($value);
+}
+
+if (! function_exists('collect')) {
+    /**
+     * Create a collection from the given value.
+     *
+     * @param  mixed  $value
+     * @return Collection
+     */
+    function collect($value = null)
+    {
+        return new Collection($value);
+    }
+}
+
+if (!function_exists('str')) {
+    /**
+     * Create a collection from the given value.
+     *
+     * @param $str string
+     * @return Corda
+     */
+    function str($str)
+    {
+        return new Corda($str);
+    }
+}
+
 function editing()
 {
     $request = Request::instance();
 
-    if (($request->routeInfo()->method() == 'onEdit' and $request->has('id')) or ($request->has('editing') or $request->attr('editing'))) {
-        return true;
+    $params = Web::editingParameters();
+    foreach ($params as $key => $param) {
+        $param_key = $param['key'];
+        $param_value = $param['value'] ?? null;
+
+        if (!$request->has($param_key) or (isset($param_value) and $request->has($param_key) and $request->get($param_key) !== $param_value)) {
+            return false;
+        }
     }
-    return false;
+    return true;
 }
