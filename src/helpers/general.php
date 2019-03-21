de029aa6e24609a86d4d09d89d8c64d602bf885d
DaviMenezes <davimenezes.dev@gmail.com>
2019-03-21 05:50:17 -0300

    remove ultimo zero apenas se o valor for diferente de zero

--- src/helpers/general.php
+++ src/helpers/general.php
@@ -87,7 +87,10 @@ function money($dollar = 0)
 {
     $dollar = empty($dollar) ? 0 : $dollar;
     $dollar = str_replace([',', '.'], '', $dollar);
-    $dollar = str($dollar)->removeLeft('00')->removeLeft('0')->str();
+    $dollar = str($dollar)->removeLeft('00');
+    if ($dollar->str() !== '0') {
+        $dollar = $dollar->removeLeft('0')->str();
+    }
     $money = Money::USD($dollar);
     return $money;
 }
