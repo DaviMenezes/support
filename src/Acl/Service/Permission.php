b1dfeb1088a554044044dee68fcb359397d92d37
DaviMenezes <davimenezes.dev@gmail.com>
2019-03-31 21:05:11 -0300

    improvements in query to permissions

--- src/Acl/Service/Permission.php
+++ src/Acl/Service/Permission.php
@@ -2,10 +2,16 @@
 
 namespace Dvi\Support\Acl\Service;
 
-use Illuminate\Database\Capsule\Manager as Capsule;
+use Dvi\Acl\Model\GroupActions;
+use Dvi\Support\Acl\Model\Groups;
+use Dvi\Support\Acl\Model\ActionTypes;
+use Dvi\Support\Acl\Model\Modules;
+use Dvi\Support\Acl\Model\UserActions;
+use Dvi\Support\Acl\Model\UserGroups;
+use FontLib\Table\Type\name;
 
 /**
- *  Permission
+ *  AclPermission
  *
  * @package
  * @subpackage
@@ -13,25 +19,73 @@ use Illuminate\Database\Capsule\Manager as Capsule;
  * @copyright  Copyright (c) 2018. (davimenezes.dev@gmail.com)
  * @see https://github.com/DaviMenezes
  */
-trait Permission
+abstract class Permission
 {
-    public static function canCreate($module = null, $user_id = null)
+    public function __call($method, $arguments)
     {
-        $modules = Capsule::table('user_rule_groups as urg')->select([
-            'urg.user_id',
-            'rg.id AS group_id',
-            'rg.NAME AS group_name',
-            'm.NAME AS module',
-            'uat.NAME AS action'])
-            ->leftJoin('rule_groups as rg', 'rg.id', '=', 'urg.rule_group_id')
-            ->leftJoin('rule_group_modules as rgm', 'rgm.group_id', '=', 'rg.id')
-            ->leftJoin('rule_group_module_actions as rgma', 'rgma.group_module_id', '=', 'rgm.id')
-            ->leftJoin('user_action_types as uat', 'uat.id', '=', 'rgma.user_action_type_id')
-            ->leftJoin('modules as m', 'm.id', '=', 'rgm.module_id')
-            ->where('urg.user_id', '=', $user_id);
+        $action = $this->prepareMethodName($method);
 
-        $result = $modules->get();
+        if ($this->methodSpecificExists($action, $arguments)) {
+            return call_user_func_array([$this, $action], $arguments);
+        }
 
-        return $result;
+        $user_id = $arguments[0];
+
+        if ($this->hasPermissionGroup($action, $user_id)) {
+            return true;
+        }
+
+        if ($this->hasUserPermission($action, $user_id)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    protected function prepareMethodName($name): string
+    {
+        $array_undescore = explode('_', $name);
+
+        if (count($array_undescore) > 1) {
+            $name = '';
+            foreach ($array_undescore as $item) {
+                $name .= ucfirst($item);
+            }
+            return $name = 'can'.$name; //ex. canCreate
+        }
+        return $name;
+    }
+
+    protected function methodSpecificExists($method_name, $arguments)
+    {
+        if (method_exists($this, $method_name)) {
+           return true;
+        }
+        return false;
+    }
+
+    protected function hasPermissionGroup($name, $user_id): bool
+    {
+        $group_result = UserGroups::db('aug')
+            ->select(['aug.user_id', 'ag.name', 'am.name', 'aat.name as action'])
+            ->join(Groups::TABLENAME.' as ag', 'aug.group_id', '=', 'ag.id')
+            ->join(GroupActions::TABLENAME.' as aga', 'ag.id', '=', 'aga.group_id')
+            ->join(ActionTypes::TABLENAME.' as aat', 'aga.action_id', '=', 'aat.id')
+            ->join(Modules::TABLENAME.' as am', 'aat.module_id', '=', 'am.id')
+            ->where('aug.user_id', '=', $user_id)
+            ->where('aat.name', '=', $name)
+            ->toSql();
+        return $group_result > 0 ? true : false;
+    }
+
+    protected function hasUserPermission($name, $user_id): bool
+    {
+        $result = UserActions::db('aup')
+            ->leftJoin(ActionTypes::TABLENAME . ' as auat', 'aup.action_id', '=', 'auat.id')
+            ->where('aup.user_id', '=', $user_id)
+            ->where('auat.name', '=', $name)
+            ->count();
+
+        return $result > 0 ? true : false;
     }
 }
